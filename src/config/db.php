74016c592d855745a0836657893d712886f77880
paulappz <pauloluyege@gmail.com>
2018-02-23 12:40:45 +0100

    upd

--- src/config/db.php
+++ src/config/db.php
@@ -3,18 +3,19 @@
 
 class db{
 
-private $dbhost='localhost';
-private $dbuser='root';
-private $dbpass='';
-private $dbname='vanhack';
+private $dbhost='us-cdbr-iron-east-05.cleardb.net';
+private $dbuser='ba0ac9d42abcff';
+private $dbpass='30c015ff';
+private $dbname='heroku_4bfbced3b843eba';
 
 //Connect
 public function connect(){
 
-    // $this->dbhost =  getenv("DB_HOSTNAME");
-    // $this->dbuser =  getenv("DB_USER");
-    // // $this->dbpass =  getenv("DB_PASS");
-    // $this->dbname =  getenv("DB_SCHEMA_CINEMA");
+  
+    // $this->dbhost =  getenv("DB_HOSTNAME_JOB");
+    // $this->dbuser =  getenv("DB_USER_JOB");
+    // // $this->dbpass =  getenv("DB_PASS_JOB");
+    // $this->dbname =  getenv("DB_SCHEMA_CINEMA_JOB");
 
 
     $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname";
