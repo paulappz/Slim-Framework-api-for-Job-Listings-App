8deb31148886c857a6becba86c0139f83a52a398
paulappz <pauloluyege@gmail.com>
2018-02-25 09:35:22 +0100

    db upd

--- src/config/db.php
+++ src/config/db.php
@@ -3,19 +3,21 @@
 
 class db{
 
-private $dbhost='us-cdbr-iron-east-05.cleardb.net';
-private $dbuser='ba0ac9d42abcff';
-private $dbpass='30c015ff';
-private $dbname='heroku_4bfbced3b843eba';
+
+
+private $dbhost;
+private $dbuser;
+private $dbpass;
+private $dbname;
 
 //Connect
 public function connect(){
 
   
-    // $this->dbhost =  getenv("DB_HOSTNAME_JOB");
-    // $this->dbuser =  getenv("DB_USER_JOB");
-    // // $this->dbpass =  getenv("DB_PASS_JOB");
-    // $this->dbname =  getenv("DB_SCHEMA_CINEMA_JOB");
+    $this->dbhost =  getenv("DB_HOSTNAME_JOB");
+    $this->dbuser =  getenv("DB_USER_JOB");
+    $this->dbpass =  getenv("DB_PASS_JOB");
+    $this->dbname =  getenv("DB_SCHEMA_CINEMA_JOB");
 
 
     $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname";
